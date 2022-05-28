de72c13c5db1c372e9b777510b027526961d2af5
sagekhis <amiralija80@gmail.com>
2022-05-28 19:16:24 +0430

    init project

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+<!--    <img src="./assets/logo.png">-->
+    <nav>
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo"> <img alt="" id="logo" src="./assets/logo.png"> </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li> <router-link to="/">Home</router-link> </li>
+          <li> <router-link to="/about">About</router-link></li>
+          <li> <a href="collapsible.html">JavaScript</a></li>
+        </ul>
+      </div>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#logo{
+  width: 50px;
+  margin: 5px;
+}
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>
+      About
+    </h1>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Expedita non suscipit tempore. Amet consequatur dolor
+      earum enim eum explicabo itaque quam, sint vero? Eius nulla sapiente, sequi sint sunt ut!
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About'
+}
+</script>
+
+<style scoped>
+
+</style>
